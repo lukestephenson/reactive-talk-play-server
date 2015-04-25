7b6b9341d6ca4399a31e6eb953b6b9e11f1bf5df
Luke Stephenson <luke.stephenson@rea-group.com>
2015-04-25 16:12:57 +1000

    Move db operations out
    so the demo isn't convoluted

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,6 @@
 package controllers
 
-import db.DatabaseOperations
-import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc._
-
-import scala.concurrent.Future
 
 object Application extends Controller {
 
@@ -12,21 +8,4 @@ object Application extends Controller {
     Ok("Hello Play")
   }
 
-  def dbOperationDbPool = Action.async { request =>
-    for {dbResult <- DatabaseOperations.loadData()}
-    yield Ok(dbResult)
-  }
-
-  def dbOperationPlayPool = Action.async { request =>
-    Future {
-      // Sleep for 1 second to simulate slow database access
-      Thread.sleep(1000)
-
-      Ok("Hello From DB")
-    }
-  }
-
-  case class Product(name:String, description: String)
-
-  
 }
---
+++ app/controllers/DBController.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import db.DatabaseOperations
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.mvc.{Action, Controller}
+
+import scala.concurrent.Future
+
+object DBController extends Controller {
+
+  def dbOperationDbPool = Action.async { request =>
+    for {dbResult <- DatabaseOperations.loadData()}
+      yield Ok(dbResult)
+  }
+
+  def dbOperationPlayPool = Action.async { request =>
+    Future {
+      // Sleep for 1 second to simulate slow database access
+      Thread.sleep(1000)
+
+      Ok("Hello From DB")
+    }
+  }
+
+}
